a1a6a94d807fdfe6b859c6c237c541ef07eb19ed
saeed6991 <saeedkhalife@gmail.com>
2023-07-06 22:19:18 +0200

    5

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
-
+import { useRouter } from 'next/router';
 import styles from './NavBar.module.css'
 import Link from 'next/link';
+import { FaSearch  } from 'react-icons/fa';
 
-const inter = Inter({ subsets: ['latin'] })
-
+const inter = Inter({ subsets: ['latin'] });
 type NavBarProps = {}
 
 const NavBar: React.FC<NavBarProps> = () => <nav className={[styles.navbar, inter.className].join(' ')}>
-  <div className={styles.logo}>OMDB</div>
+  <Link href="/"><div className={styles.logo}>OMDB</div></Link>
   <ul className={styles.navLinks}>
     <div className={styles.menu}>
-      <li><Link href="/">Search</Link></li>
+      <li><Link href="/"> <FaSearch  className={styles.searchIcon} /></Link></li>
       <li><Link href="/recommend">Recommendations</Link></li>
     </div>
   </ul>
